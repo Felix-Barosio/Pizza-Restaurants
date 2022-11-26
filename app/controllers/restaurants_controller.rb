bc7cd73119e8fab4a0b220eb73f3508f97307060
Felix <barosiofelix@gmail.com>
2022-11-26 16:54:43 +0300

    Added show method

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,31 @@
 class RestaurantsController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    wrap_parameters format: []
+
     def index
         render json: Restaurant.all
+    end
+
+    def show
+        restaurant = find_restaurant
+        render json: restaurant, serializer: ShowRestaurantSerializer
+    end
+
+
+    private
+
+    def restaurant_params
+        params.permit(:name, :address)
+    end
+
+    def find_restaurant
+        Restaurant.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { errors: "Restaurant not found"}, status: :not_found
     end
 
 end
