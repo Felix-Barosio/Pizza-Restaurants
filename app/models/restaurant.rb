239ce39ee99a6045d74610d5da98e417b2a6bff3
Felix <barosiofelix@gmail.com>
2022-12-01 14:39:02 +0300

    Added validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas, dependent: :destroy
     has_many :pizzas, through: :restaurant_pizzas
 
+    validates :address, presence: true
+    validates :name, presence: true
+
 end
