d3a6127f4c3b9b1a54a7e884495832a27331b235
Felix <barosiofelix@gmail.com>
2022-12-01 13:12:55 +0300

    Added create and update

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     wrap_parameters format: []
 
@@ -13,22 +14,40 @@ class RestaurantsController < ApplicationController
         render json: restaurant, serializer: ShowRestaurantSerializer
     end
 
+    def create
+        new_restaurant = Restaurant.create!(restaurant_params)
+        render json: new_restaurant, status: :created
+    end
+
+    def update
+        restaurant = find_restaurant
+        restaurant.update!(restaurant_params)
+        render json: restaurant, status: :ok
+    end
+
     def destroy
         restaurant = find_restaurant
         restaurant.destroy
-        render json: {}, status: :no_content
+        render json: { message: "Pizza Restaurant Deleted" }, status: :no_content
     end
 
 
     private
 
-    
+    def restaurant_params
+        params.permit(:address)
+    end
+
     def find_restaurant
         Restaurant.find(params[:id])
     end
 
     def render_not_found_response
         render json: { errors: "Restaurant not found"}, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
